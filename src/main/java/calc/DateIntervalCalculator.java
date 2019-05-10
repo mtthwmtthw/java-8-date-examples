8c082f9df623703033f7c67f15954aec201c6e01
mtthwmtthw <mtthwfolk@gmail.com>
2019-05-10 01:31:45 -0400

    Adding interval and range calculator of dates

---
+++ src/main/java/calc/DateIntervalCalculator.java
@@ -0,0 +1,36 @@
+package calc;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+import static java.time.temporal.ChronoUnit.DAYS;
+
+public class DateIntervalCalculator {
+
+	public static boolean returnIfIntervalByDateAndInteger(LocalDate startingDate, LocalDate testDate, int interval) {
+		int length = Period.between(testDate, startingDate).getDays();
+		
+		if (length % interval == 0) {
+			return true;
+		}	
+		
+		return false;
+	}
+	
+	public static List<LocalDate> returnIntervalDatesByRange(LocalDate startingDate, LocalDate endDate, int interval) {
+		List<LocalDate> dates = new ArrayList<LocalDate>();
+		int length = (int)DAYS.between(startingDate, endDate);
+		int occurences = length / interval;
+		LocalDate dateIndex = startingDate;
+		
+		for(int i = 0; i < occurences; i++) {
+			LocalDate nextDate = dateIndex.plusDays(interval);
+			dates.add(nextDate);
+			dateIndex = nextDate;
+		}
+		
+		return dates;
+	}
+	
+}
---
+++ src/test/java/calc/DateIntervalCalculatorTest.java
@@ -0,0 +1,61 @@
+package calc;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class DateIntervalCalculatorTest {
+
+	@Test
+	public void testIfIntervalOfDateByInteger() throws ParseException {
+		LocalDate startDate = LocalDate.parse("2019-01-01");
+		LocalDate testDate = LocalDate.parse("2019-01-31");
+		LocalDate anotherTestDate = LocalDate.parse("2019-03-01");
+		
+		int interval = 30;
+		
+		assertTrue(DateIntervalCalculator.returnIfIntervalByDateAndInteger(startDate, testDate, interval));
+		assertTrue(DateIntervalCalculator.returnIfIntervalByDateAndInteger(startDate, anotherTestDate, interval));
+		
+	}
+	
+	@Test
+	public void testReturnDatesByIntervalAndRange() throws ParseException {
+		int interval = 90;
+		LocalDate startDate = LocalDate.parse("2019-01-01");
+		LocalDate endDate = LocalDate.parse("2024-01-31");
+		String dates = "2019-04-01\n" + 
+				"2019-06-30\n" + 
+				"2019-09-28\n" + 
+				"2019-12-27\n" + 
+				"2020-03-26\n" + 
+				"2020-06-24\n" + 
+				"2020-09-22\n" + 
+				"2020-12-21\n" + 
+				"2021-03-21\n" + 
+				"2021-06-19\n" + 
+				"2021-09-17\n" + 
+				"2021-12-16\n" + 
+				"2022-03-16\n" + 
+				"2022-06-14\n" + 
+				"2022-09-12\n" + 
+				"2022-12-11\n" + 
+				"2023-03-11\n" + 
+				"2023-06-09\n" + 
+				"2023-09-07\n" + 
+				"2023-12-06";
+		String[] dateArray = dates.split("\\n");
+		LocalDate[] parsedDates = new LocalDate[dateArray.length];
+		
+		for(int i = 0; i < dateArray.length; i++) {
+			parsedDates[i] = LocalDate.parse(dateArray[i]);
+		}
+		
+		assertArrayEquals(parsedDates, DateIntervalCalculator.returnIntervalDatesByRange(startDate, endDate, interval).toArray());
+		
+	}
+}
